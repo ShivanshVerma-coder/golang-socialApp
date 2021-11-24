4152c1779e142661f6365080f112582512b888a7
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-11-24 21:59:16 +0530

    updated

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,12 +7,23 @@ import (
 
 	"github.com/ShivanshVerma-coder/golang-socialApp/pkg/routes"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
 	fmt.Println("Main ran")
-	r := mux.NewRouter()
 
-	routes.InitializeRoutes(r)                 // Initialize all routes
-	log.Fatal(http.ListenAndServe(":8080", r)) // start server
+	r := mux.NewRouter() //router
+
+	//Cors
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8000"},
+		AllowCredentials: true, // set this to true if your api supports cookies
+		AllowedHeaders:   []string{"*"},
+	})
+
+	handler := c.Handler(r)
+
+	routes.InitializeRoutes(r)                       // Initialize all routes
+	log.Fatal(http.ListenAndServe(":8080", handler)) // start server
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.8.0
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
 	gorm.io/driver/mysql v1.2.0
 	gorm.io/gorm v1.22.3
